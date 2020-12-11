e19d7ba40fe8083df9ac901dd549cd510925c45e
Chris Messa <blindsun95@gmail.com>
2020-12-11 14:36:34 -0500

    the song has been written

---
+++ 99Bottles.js
@@ -0,0 +1,11 @@
+for (var i = 99; i > 0; i--) {
+  console.log(
+    i +
+      " bottles of beer on the wall, " +
+      i +
+      " bottles of beer" +
+      "\n" +
+      "Take one down, pass it around"
+  );
+  console.log(i - 1 + " bottles of beer on the wall!");
+}
